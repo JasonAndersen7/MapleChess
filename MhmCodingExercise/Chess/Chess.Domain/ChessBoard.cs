51c6ec2a4ac3cbd9da29c2fe2def9339fe4e6f1b
jasonAndersen7 <andersen.jason7@gmail.com>
2017-07-29 13:12:14 -0600

    Legal Board Implementation
    Implement method

--- MhmCodingExercise/Chess/Chess.Domain/ChessBoard.cs
+++ MhmCodingExercise/Chess/Chess.Domain/ChessBoard.cs
@@ -8,19 +8,48 @@ namespace Chess.Domain
         public static readonly int MaxBoardHeight = 7;
         private Pawn[,] pieces;
 
-        public ChessBoard ()
+        public ChessBoard()
         {
             pieces = new Pawn[MaxBoardWidth, MaxBoardHeight];
         }
 
         public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor)
         {
-            throw new NotImplementedException("Need to implement ChessBoard.Add()");
+            try
+            {
+                pawn.XCoordinate = xCoordinate;
+                pawn.YCoordinate = yCoordinate;
+                pieces[xCoordinate, yCoordinate] = pawn;
+            }
+            catch
+            {
+                throw;
+            }
+           // throw new NotImplementedException("Need to implement ChessBoard.Add()");
         }
 
         public bool IsLegalBoardPosition(int xCoordinate, int yCoordinate)
         {
-            throw new NotImplementedException("Need to implement ChessBoard.IsLegalBoardPosition()");
+            //set true as the default value
+            //this will force the method to validate all potential future cases
+            bool result = true;
+            //if x fails, return it
+            //since you are using a 0 index, adjust the max board width
+            if (xCoordinate < 0 || xCoordinate > MaxBoardWidth-1)
+            {
+                result = false;
+                return result;
+            }
+            //if y fails return it
+            //since you are using a 0 index, adjust the max board height
+            if (yCoordinate < 0 || yCoordinate > MaxBoardHeight)
+            {
+                result = false;
+                return result;
+            }
+            return result;
+
+            //throw new NotImplementedException("Need to implement ChessBoard.IsLegalBoardPosition()");
         }
 
     }
